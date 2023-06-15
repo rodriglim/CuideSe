a81f7d984bebfa50e4c4ad97df7fd3dd6995c7d7
Rodrigo Lima <109808618+rodriglim@users.noreply.github.com>
2023-06-15 16:19:56 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,35 +1,14 @@
-document.onscroll = () => {
+document.onscroll= () => {
     var nav = document.getElementById('nav');
     window.addEventListener("scroll", function (event) {
         if (window.scrollY <= 500) {
             nav.style.background = "rgba(0, 255, 121, 0.3)";
-
+            
         }
-        else {
-            nav.style.background = "rgba(0, 255, 121, 0.8)";
+        else {       
+             nav.style.background = "rgba(0, 255, 121, 0.8)";
         }
     });
 }
 
-
-
-const $veganosMenu = document.querySelector('#veganos');
-const $lactoseMenu = document.querySelector('#semLactose');
-window.idBotaoClicado = '';
-
-function $buttonSelection(e) {
-
-    window.idBotaoClicado = e
-   
-    if(idBotaoClicado == 'vegan'){
-        $veganosMenu.style.display = 'block' ;
-        $lactoseMenu.style.display= 'none';
-        
-    }
-    else if(idBotaoClicado == 'lactose'){
-        $lactoseMenu.style.display = 'block';
-        $veganosMenu.style.display = 'none';
-     }
-
-}
 
